08032d2f265156cda375b5210f2100fe71aa45f3
simple0628 <simple0628@users.noreply.github.com>
2026-04-26 22:39:35 +0800

    v0.1.6

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ import { getApiKey, resetConfig, loadSkills, SKILLS_DIR } from "./config.js";
 import { buildSystemPrompt, chatRound, saveMemory } from "./chat.js";
 import { resetInterrupt, triggerInterrupt, getSignal } from "./state.js";
 
-const VERSION = "0.1.5";
+const VERSION = "0.1.6";
 
 // Markdown 终端渲染
 marked.use(markedTerminal() as Parameters<typeof marked.use>[0]);
